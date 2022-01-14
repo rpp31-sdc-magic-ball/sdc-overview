b23be7efee1f50c9282a6f549b470fea97ccf099
Poyraz <poyraztcc@gmail.com>
2022-01-13 17:32:36 -1000

    Stress testing

---
+++ tests/stress/10rps.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 0 } // scale down. Recovery stage.
+  ],
+};
+
+export default function () {
+  const BASE_URL = 'http://localhost:3001'; // make sure this is not production
+
+  const responses = http.batch([
+    ['GET', `${BASE_URL}/products/50000/styles`, null, { tags: { name: 'SDC' } }],
+  ]);
+
+  sleep(1);
+}
